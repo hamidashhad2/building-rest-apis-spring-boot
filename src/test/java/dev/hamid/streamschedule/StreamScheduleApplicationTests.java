a8b833f7fc1371eb03b738dabf92bcd677bfc951
HAMID ASHHAD <hamidashhad11@gmail.com>
2022-08-22 22:34:15 +0500

    initial commit

---
+++ src/test/java/dev/hamid/streamschedule/StreamScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hamid.streamschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
