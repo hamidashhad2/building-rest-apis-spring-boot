361fafe8bccdc756977d2bcbca9a4120c6908467
HAMID ASHHAD <hamidashhad11@gmail.com>
2022-08-22 23:39:21 +0500

    different constructors demonstration

--- src/main/java/dev/hamid/streamschedule/model/LiveStream.java
+++ src/main/java/dev/hamid/streamschedule/model/LiveStream.java
@@ -9,4 +9,20 @@ public record LiveStream( String id,
          LocalDateTime startDate,
          LocalDateTime endDate) {
 
+    //canonical constructor
+    /*public LiveStream(String id, String title, String description, String url, LocalDateTime startDate, LocalDateTime endDate) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.url = url;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }*/
+
+    // compact constructor
+    /*public LiveStream {
+        if(id == null) {
+            throw new IllegalArgumentException("Id cannot be null");
+        }
+    }*/
 }
